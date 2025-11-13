bb60d978c19e6689bf427588d34048433f75d9d0
Bellal hossain <164983736+ak0384221@users.noreply.github.com>
2025-11-13 14:24:14 +0600

    Update Header.jsx
    updated experience

--- app/(home)/components/Header.jsx
+++ app/(home)/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
           MD Bellal hossain
         </h1>
         <p className={`${saira.className} font-extralight text-sm`}>
-          Frontend developer | 15 months of experience <br />
+          Frontend developer | 1.5+ years of experience <br />
           React | Next | Redux
         </p>
       </div>

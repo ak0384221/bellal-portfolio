30911b0f115e0dcaab4ac4dc966b793251bef6d6
Ayaan <sharingan0384221@gmail.com>
2026-08-02 11:52:29 +0600

    updated title

--- app/(home)/components/Header.jsx
+++ app/(home)/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
         {/* Badge */}
         <div className="my-2 inline-flex items-center gap-2 rounded-full bg-cyan-400/5 px-4 py-2 text-[11px] text-cyan-300">
           <span className="h-2 w-2 rounded-full bg-cyan-400 animate-pulse" />
-          Data Analyst • ETL Engineer • Developer
+          System Admin • Data Analyst • ETL Engineer • Developer
         </div>
       </div>
     </header>
